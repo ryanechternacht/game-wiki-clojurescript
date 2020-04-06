45d8306fbc76e09a2e215ecc0bf380ed3703795c
ryanechternacht <ryan@echternacht.org>
2020-04-05 23:49:03 -0400

    Make the barchart more advanced

--- src/cljs/game_wiki_clojurescript/d3/views.cljs
+++ src/cljs/game_wiki_clojurescript/d3/views.cljs
@@ -4,30 +4,59 @@
             [rid3.core :as rid3 :refer [rid3->]]
             [goog.object :as gobj]))
 
-(def dimensions {:width 200
-                 :height 200})
+;;TODO get this in ratom somehow
+(def chart {:canvas {:width 200
+                     :height 200}
+            :title "My Chart"})
 
+(def margin {:top 30 ;; title
+             :bottom 20 ;; y axis
+             :left 24 ;; x-axis
+             :right 0})
+
+(defn make-chart-area [chart]
+  (let [{canvas :canvas} chart]
+    {:x (:left margin)
+     :y (:top margin)
+     :width (- (:width canvas) (:left margin) (:right margin))
+     :height (- (:height canvas) (:top margin) (:bottom margin))}))
+
+(def canvas (:canvas chart))
+
+;;TOOO build from chart
+(def chart-area (make-chart-area chart))
 
 (def data
   {:dataset [{:label "A" :value 10}
              {:label "B" :value 20}
-             {:label "C" :value 30}]})
+             {:label "C" :value 35}
+             {:label "D" :value 50}]})
+
+(defn translate [x y]
+  (let [x1 (if (nil? x) 0 x)
+        y1 (if (nil? y) 0 y)]
+    (str "translate(" x "," y ")")))
 
 (defn prepare-dataset [ratom]
   (-> @ratom
       :dataset
       clj->js))
 
-(def color
-  (-> js/d3
-      (.scaleOrdinal #js ["red" "green" "blue"])))
+(def bar-colors ["red" "green" "blue"])
+
+(defn make-bar-color-selector [bar-colors]
+  (fn [d i]
+    (->> (rem i (count bar-colors))
+         (nth bar-colors))))
+
+(def ->bar-color (make-bar-color-selector bar-colors))
 
 (defn ->x-scale [ratom]
   (let [{:keys [dataset]} @ratom
         labels (mapv :label dataset)]
     (-> js/d3
         .scaleBand
-        (.rangeRound #js [0 (:width dimensions)])
+        (.rangeRound #js [0 (:width chart-area)])
         (.padding 0.1)
         (.domain (clj->js labels)))))
 
@@ -37,7 +66,7 @@
         max-value (apply max values)]
     (-> js/d3
         .scaleLinear
-        (.rangeRound #js [(:height dimensions) 0])
+        (.rangeRound #js [(:height chart-area) 0])
         (.domain #js [0 max-value]))))
 
 (defn viz [{:keys [ratom]}]
@@ -47,8 +76,12 @@
     :svg {:did-mount
           (fn [node ratom]
             (rid3-> node
-                    {:width (:width dimensions)
-                     :height (:height dimensions)}))}
+                    {:width (:width canvas)
+                     :height (:height canvas)}))}
+    :main-container {:did-mount
+                     (fn [node ratom]
+                       (rid3-> node
+                               {:transform (translate (:x chart-area) (:y chart-area))}))}
     :pieces [{:kind :elem-with-data
               :class "bars"
               :tag "rect"
@@ -63,9 +96,35 @@
                            :y (fn [d]
                                 (y-scale (gobj/get d "value")))
                            :width (.bandwidth x-scale)
-                           :fill (fn [d i] (color i))
+                           :fill ->bar-color
                            :height (fn [d]
-                                     (- (:height dimensions) (y-scale (gobj/get d "value"))))})))}]}])
+                                     (- (:height chart-area) (y-scale (gobj/get d "value"))))})))}
+             {:kind :container
+              :class "x-axis"
+              :did-mount
+              (fn [node ratom]
+                (let [x-scale (->x-scale ratom)]
+                  (rid3-> node
+                          {:transform (translate 0 (:height chart-area))}
+                          (.call (.axisBottom js/d3 x-scale)))))}
+             {:kind :container
+              :class "y-axis"
+              :did-mount
+              (fn [node ratom]
+                (let [y-scale (->y-scale ratom)]
+                  (rid3-> node
+                          (.call (-> (.axisLeft js/d3 y-scale)
+                                     (.ticks 3))))))}
+             {:kind :elem
+              :class "title"
+              :tag "text"
+              :prepare-dataset :title
+              :did-mount
+              (fn [node ratom]
+                (rid3-> node
+                        {:x 0
+                         :y -10}
+                        (.text (:title chart))))}]}])
 
 (defn d3-overview-page []
   (let [ratom (r/atom data)]
@@ -73,4 +132,3 @@
      [:h3 "d3 Demo"]
      (let [data (r/atom data)]
        [viz {:ratom data}])]))
-
