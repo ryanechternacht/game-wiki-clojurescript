18f17cabfeeb75f80bbde7df2d5463c07da6a7f9
ryanechternacht <ryan@echternacht.org>
2020-04-06 00:24:11 -0400

    Cleanup usage patterns a lot

--- src/cljs/game_wiki_clojurescript/d3/views.cljs
+++ src/cljs/game_wiki_clojurescript/d3/views.cljs
@@ -4,34 +4,15 @@
             [rid3.core :as rid3 :refer [rid3->]]
             [goog.object :as gobj]))
 
-;;TODO get this in ratom somehow
-(def chart {:canvas {:width 200
-                     :height 200}
-            :title "My Chart"})
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Chart Params
 (def margin {:top 30 ;; title
              :bottom 20 ;; y axis
              :left 24 ;; x-axis
              :right 0})
 
-(defn make-chart-area [chart]
-  (let [{canvas :canvas} chart]
-    {:x (:left margin)
-     :y (:top margin)
-     :width (- (:width canvas) (:left margin) (:right margin))
-     :height (- (:height canvas) (:top margin) (:bottom margin))}))
-
-(def canvas (:canvas chart))
-
-;;TOOO build from chart
-(def chart-area (make-chart-area chart))
-
-(def data
-  {:dataset [{:label "A" :value 10}
-             {:label "B" :value 20}
-             {:label "C" :value 35}
-             {:label "D" :value 50}]})
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Utils
 (defn translate [x y]
   (let [x1 (if (nil? x) 0 x)
         y1 (if (nil? y) 0 y)]
@@ -42,17 +23,29 @@
       :dataset
       clj->js))
 
-(def bar-colors ["red" "green" "blue"])
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Build chart components
+;; All of these take ratom,
+;; 1) this is simple
+;; 2) does this cause unecessary redrawing?
+;; 3) should they only take the direct thing they need?
+(defn ->chart-area [ratom]
+  (let [{canvas :canvas} @ratom]
+    {:x (:left margin)
+     :y (:top margin)
+     :width (- (:width canvas) (:left margin) (:right margin))
+     :height (- (:height canvas) (:top margin) (:bottom margin))}))
 
-(defn make-bar-color-selector [bar-colors]
-  (fn [d i]
-    (->> (rem i (count bar-colors))
-         (nth bar-colors))))
-
-(def ->bar-color (make-bar-color-selector bar-colors))
+; need default bar-colors
+(defn ->bar-color [ratom]
+  (let [bar-colors (get-in @ratom [:canvas :bar-colors])]
+    (fn [i]
+      (->> (rem i (count bar-colors))
+           (nth bar-colors)))))
 
 (defn ->x-scale [ratom]
   (let [{:keys [dataset]} @ratom
+        chart-area (->chart-area ratom)
         labels (mapv :label dataset)]
     (-> js/d3
         .scaleBand
@@ -62,6 +55,7 @@
 
 (defn ->y-scale [ratom]
   (let [{:keys [dataset]} @ratom
+        chart-area (->chart-area ratom)
         values (mapv :value dataset)
         max-value (apply max values)]
     (-> js/d3
@@ -75,13 +69,15 @@
     :ratom ratom
     :svg {:did-mount
           (fn [node ratom]
-            (rid3-> node
-                    {:width (:width canvas)
-                     :height (:height canvas)}))}
+            (let [{canvas :canvas} @ratom]
+              (rid3-> node
+                      {:width (:width canvas)
+                       :height (:height canvas)})))}
     :main-container {:did-mount
                      (fn [node ratom]
-                       (rid3-> node
-                               {:transform (translate (:x chart-area) (:y chart-area))}))}
+                       (let [chart-area (->chart-area ratom)]
+                         (rid3-> node
+                                 {:transform (translate (:x chart-area) (:y chart-area))})))}
     :pieces [{:kind :elem-with-data
               :class "bars"
               :tag "rect"
@@ -89,21 +85,24 @@
               :did-mount
               (fn [node ratom]
                 (let [y-scale (->y-scale ratom)
-                      x-scale (->x-scale ratom)]
+                      x-scale (->x-scale ratom)
+                      chart-area (->chart-area ratom)
+                      bar-color-fn (->bar-color ratom)]
                   (rid3-> node
                           {:x (fn [d]
                                 (x-scale (gobj/get d "label")))
                            :y (fn [d]
                                 (y-scale (gobj/get d "value")))
                            :width (.bandwidth x-scale)
-                           :fill ->bar-color
+                           :fill (fn [d i] (bar-color-fn i))
                            :height (fn [d]
                                      (- (:height chart-area) (y-scale (gobj/get d "value"))))})))}
              {:kind :container
               :class "x-axis"
               :did-mount
               (fn [node ratom]
-                (let [x-scale (->x-scale ratom)]
+                (let [x-scale (->x-scale ratom)
+                      chart-area (->chart-area ratom)]
                   (rid3-> node
                           {:transform (translate 0 (:height chart-area))}
                           (.call (.axisBottom js/d3 x-scale)))))}
@@ -118,17 +117,29 @@
              {:kind :elem
               :class "title"
               :tag "text"
-              :prepare-dataset :title
               :did-mount
               (fn [node ratom]
                 (rid3-> node
                         {:x 0
                          :y -10}
-                        (.text (:title chart))))}]}])
+                        (.text (get-in @ratom [:canvas :title]))))}]}])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Usage
+(def dataset
+  [{:label "A" :value 10}
+   {:label "B" :value 20}
+   {:label "C" :value 35}
+   {:label "D" :value 50}])
+
+(def canvas {:title "My Chart"
+             :width 200
+             :height 200
+             :bar-colors ["red" "green" "blue"]})
 
 (defn d3-overview-page []
-  (let [ratom (r/atom data)]
+  (let [ratom (r/atom {:dataset dataset
+                       :canvas canvas})]
     [:div
      [:h3 "d3 Demo"]
-     (let [data (r/atom data)]
-       [viz {:ratom data}])]))
+     [viz {:ratom ratom}]]))
